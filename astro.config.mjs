214eb16c01797eab44c3e3ef95d27ad8f3214d89
Jordan Walker <walker.jlg@gmail.com>
2025-03-19 07:11:51 +0000

    Add home and about pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import 'dotenv/config'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: process.env.SITE,
+  base: process.env.BASE_URL,
+  trailingSlash: 'ignore',
+  devToolbar: {
+    enabled: false,
+  },
+  vite: {
+    css: {
+      devSourcemap: true,
+    },
+  },
+})
---
+++ src/consts.ts
@@ -0,0 +1,3 @@
+export const SITE_TITLE = "Poplett's Dog Grooming"
+export const SITE_DESCRIPTION =
+  'Professional dog grooming in Bradford, UK. Our experienced team offers gentle, affordable dog grooming services for all breeds. Book your appointment today.'
